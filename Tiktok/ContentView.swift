e97f5595bc5b5fe6b1234db1db1f791551844768
davalcato <davalcato@yahoo.com>
2020-05-28 14:46:57 -0700

    added the import AVKit along with the videos path way in the ContentView

--- Tiktok/ContentView.swift
+++ Tiktok/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
     var body: some View {
@@ -227,3 +228,23 @@ class Host: UIHostingController<ContentView> {
     }
     
 }
+
+// This is the video area...
+
+struct Video : Identifiable {
+    
+    var id : Int
+    var player : AVPlayer
+    var replay : Bool
+}
+
+var data = [
+    
+    Video(id: 0, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video4", ofType: "mp4")!)), replay: false),
+    Video(id: 1, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video5", ofType: "mp4")!)), replay: false),
+    Video(id: 2, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video2", ofType: "mov")!)), replay: false),
+    Video(id: 3, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video3", ofType: "mp4")!)), replay: false),
+    Video(id: 4, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video1", ofType: "mov")!)), replay: false),
+    Video(id: 5, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video6", ofType: "mp4")!)), replay: false),
+    
+]
